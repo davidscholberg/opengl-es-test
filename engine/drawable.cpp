dabc2617b6b8e63137e9855dd8089a69758c9791
David Scholberg <recombinant.vector@gmail.com>
2018-07-21 14:32:44 -0400

    formatting fix

--- engine/drawable.cpp
+++ engine/drawable.cpp
@@ -6,9 +6,9 @@ drawable::drawable(const std::vector<float> &vertex_vector, const int vertex_dep
     : vertices(vertex_vector), vertex_depth(vertex_depth), program(program) {
     this->vertex_count = vertex_vector.size() / this->vertex_depth / 2;
 
-	this->position_attrib = this->program->get_attrib_location("position");
-	this->color_attrib = this->program->get_attrib_location("color");
-	this->offset_uniform = this->program->get_uniform_location("offset");
+    this->position_attrib = this->program->get_attrib_location("position");
+    this->color_attrib = this->program->get_attrib_location("color");
+    this->offset_uniform = this->program->get_uniform_location("offset");
 }
 
 void drawable::update_offsets(float dx, float dy, float dz) {
@@ -21,8 +21,8 @@ void drawable::draw() {
     this->program->use();
     this->vertices.bind();
 
-	glEnableVertexAttribArray(this->position_attrib);
-	glVertexAttribPointer(
+    glEnableVertexAttribArray(this->position_attrib);
+    glVertexAttribPointer(
             this->position_attrib,
             this->vertex_depth,
             GL_FLOAT,
@@ -30,14 +30,14 @@ void drawable::draw() {
             0,
             0);
 
-	glEnableVertexAttribArray(this->color_attrib);
-	glVertexAttribPointer(
-			this->color_attrib,
-			this->vertex_depth,
-			GL_FLOAT,
-			GL_FALSE,
-			0,
-			(GLvoid*) (sizeof(float) * this->vertex_depth * this->vertex_count));
+    glEnableVertexAttribArray(this->color_attrib);
+    glVertexAttribPointer(
+            this->color_attrib,
+            this->vertex_depth,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            (GLvoid*) (sizeof(float) * this->vertex_depth * this->vertex_count));
 
     glUniform3f(this->offset_uniform, this->offset_x, this->offset_y, this->offset_z);
     glDrawArrays(GL_TRIANGLE_FAN, 0, this->vertex_count);
